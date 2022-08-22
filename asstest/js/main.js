4a371949a9c937c9ba02a680e94fecad8de46987
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-08-22 14:29:39 +0200

    commit

---
+++ asstest/js/main.js
@@ -0,0 +1,28 @@
+const headline = ('CodeFlow Übung lev1_2: Date Methods')
+document.getElementById('headline').innerHTML = headline
+
+
+let monate = [
+    "Januar",
+    "Februar",
+    "März",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Dezember"
+]
+
+let wochenTag = [
+    "Sonntag",
+    "Montag",
+    "Dienstag",
+    "Mittwoch",
+    "Donnerstag",
+    "Freitag",
+    "Samstag"
+]
